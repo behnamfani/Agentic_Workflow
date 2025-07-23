48bc88ed0ee6d4f33b377b8e7b6f479b15c3e8c1
Behnam Fanitabasi <behnam.fani18@gmail.com>
2025-07-23 23:29:05 +0200

    Settings added

--- Examples/Groq.py
+++ Examples/Groq.py
@@ -1,24 +1,29 @@
 import os
 from langchain_groq import ChatGroq
 
-os.environ["GROQ_API_KEY"] = "..."
+from src.config import settings, logger
 
-llm = ChatGroq(
-    model="deepseek-r1-distill-llama-70b",
-    temperature=0.5,
-    max_tokens=None,
-    reasoning_format="parsed",
-    timeout=None,
-    max_retries=2,
-)
+os.environ["GROQ_API_KEY"] = settings.GROQ_API_KEY
 
-messages = [
-    (
-        "system",
-        "You are a helpful assistant that translates English to French. Translate the user sentence. "
-        "Then give me word to word translations",
-    ),
-    ("human", "I love programming and Pandas."),
-]
-ai_msg = llm.invoke(messages)
-print(ai_msg)
+try:
+    llm = ChatGroq(
+        model="deepseek-r1-distill-llama-70b",
+        temperature=0.5,
+        max_tokens=None,
+        reasoning_format="parsed",
+        timeout=None,
+        max_retries=2,
+    )
+
+    messages = [
+        (
+            "system",
+            "You are a helpful assistant that translates English to French. Translate the user sentence. "
+            "Then give me word to word translations",
+        ),
+        ("human", "I love programming and Pandas."),
+    ]
+    ai_msg = llm.invoke(messages)
+    print(ai_msg)
+except Exception as e:
+    logger.error(f"Error at generating response: {e}")
---
+++ src/config.py
@@ -0,0 +1,37 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+from typing import Optional
+from pydantic import Field
+
+load_dotenv(dotenv_path="../.env")
+
+
+class Settings(BaseSettings):
+    MODEL_NAME: str = "deepseek-r1-distill-llama-70b"  # meta-llama/Llama-4-Scout-17B-16E-Instruct
+    MAX_TOKENS: int = 1024
+    TEMPERATURE: float = 0.3
+    TIMEOUT: float = 10.0  # groq is fast
+    TOP_P: float = 0.95
+    STREAM: bool = False
+    MAX_RETERIES: int = 2
+    GROQ_API_KEY: Optional[str] = None
+    GROQ_ENDPOINT: Optional[str] = None
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+        extra = "allow"
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s:%(lineno)d - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+settings = Settings()
+
+logger.info("Environment configuration loaded successfully")
