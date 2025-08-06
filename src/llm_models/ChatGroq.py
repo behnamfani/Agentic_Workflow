4d666971c155a2111c15a63c4bab7aac2d0cac7f
Behnam Fanitabasi <behnam.fani18@gmail.com>
2025-08-06 23:29:13 +0200

    Groqchatbot added

---
+++ src/llm_models/ChatGroq.py
@@ -0,0 +1,43 @@
+import os
+from langchain_groq import ChatGroq
+
+from src.config import settings, logger
+
+os.environ["GROQ_API_KEY"] = settings.GROQ_API_KEY
+
+
+class Groq:
+
+    def __init__(self, system_text="You are an helpful AI assistant."):
+        """
+        Create Groq chat llm given system text
+        """
+        try:
+            self.system_text = system_text
+            self.llm = ChatGroq(
+                model=settings.MODEL_NAME,
+                temperature=settings.TEMPERATURE,
+                max_tokens=settings.MAX_TOKENS,
+                reasoning_format="parsed",
+                timeout=settings.TIMEOUT,
+                max_retries=settings.MAX_RETERIES,
+            )
+        except Exception as e:
+            logger.error(f"Error at creating model: {e}")
+
+    def ask(self, query: str) -> str:
+        """
+        Generate response based on the system text and given query
+        :param query: given query
+        :return:
+        """
+        try:
+            messages = [
+                ("system", self.system_text),
+                ("human", query),
+            ]
+            response = self.llm.invoke(messages)
+            return response.content
+        except Exception as e:
+            logger.error(f"Error at generating response: {e}")
+            return f"Error at generating response: {e}"
