c279631bbd42d9def70332fcb47b9fe497bd5096
Behnam Fanitabasi <behnam.fani18@gmail.com>
2026-04-14 00:18:35 +0200

    knowledge tool added

--- src/tools/knowledge.py
+++ src/tools/knowledge.py
@@ -1,3 +1,6 @@
+from typing import Tuple, Type
+
+from langchain_community.tools.semanticscholar import SemanticScholarQueryRun
 from langchain_community.tools.semanticscholar.tool import SemanticScholarQueryRun
 from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
@@ -6,15 +9,18 @@ from langchain_core.tools import StructuredTool
 
 wikipedia = None
 
+
 def wikipedia_search(query: str) -> str:
     global wikipedia
     if wikipedia is None:
         wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
     return wikipedia.run(query)
 
+
 def get_knowledge_tools() -> list:
     tools = [SemanticScholarQueryRun(), WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())]
     return tools
 
-def get_tools() -> tuple[StructuredTool, StructuredTool]:
+
+def get_tools() -> tuple[Type[SemanticScholarQueryRun], StructuredTool]:
     return SemanticScholarQueryRun, StructuredTool.from_function(wikipedia_search)
